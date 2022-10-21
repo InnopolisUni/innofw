bc1c8620a373eb0afbd194d305539b2304d613ca
Shamil Khastiev <s.hastiev@innopolis.university>
2022-10-21 15:42:02 +0300

    add auto cd

--- examples/full_mem_loading.sh
+++ examples/full_mem_loading.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 sudo apt-get install -y iotop > /dev/null
+PWD=$(pwd)
+if [ "${PWD##*/}"="examples" ]; then
+  cd ..
+fi
 nohup python train.py experiments=KA_130722_9f7134db_linear_regression.yaml &
 PID=$!
 sudo iotop -bot -d 0.1 | grep $PID
