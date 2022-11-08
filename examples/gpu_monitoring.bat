6237052cf7183369700df995c7178193063818fb
Kazybek Askarbek <k.askarbek@innopolis.university>
2022-11-08 14:10:15 +0300

    added linux and windows scripts for training with gpu

---
+++ examples/gpu_monitoring.bat
@@ -0,0 +1,2 @@
+start procexp.exe &
+python train.py experiments=IM_190722_vwer3f23_oneshotlearning.yaml optimizers=auto accelerator=gpu +devices=1
--- examples/gpu_monitoring.sh
+++ examples/gpu_monitoring.sh
@@ -1,14 +1,8 @@
-##!/bin/bash
-#sudo add-apt-repository ppa:flexiondotorg/nvtop -y > /dev/null
-#sudo apt install nvtop > /dev/null
-#PWD=$(pwd)
-#if [ "${PWD##*/}"="examples" ]; then
-#  cd ..
-#fi
-#nohup
-python train.py experiments=IM_190722_vwer3f23_oneshotlearning.yaml optimizers=auto accelerator=gpu +devices=1 &
-# &
-#PID=$!
-#sudo nvtop
- #-bot -d 0.1 | grep $PID
-#kill $PID
+#!/bin/bash
+sudo add-apt-repository ppa:flexiondotorg/nvtop -y > /dev/null
+sudo apt install nvtop > /dev/null
+
+nohup python train.py experiments=IM_190722_vwer3f23_oneshotlearning.yaml optimizers=auto accelerator=gpu +devices=1 &
+PID=$!
+sudo nvtop
+kill $PID
