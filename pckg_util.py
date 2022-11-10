aef7900c9ca78950f0d1bbc8e903d81391ebac6a
ibragimmergaliev <i.mergaliev@innopolis.ru>
2022-11-10 15:55:09 +0300

    Removed print

--- pckg_util.py
+++ pckg_util.py
@@ -39,14 +39,12 @@ def check_gpu_and_torch_compatibility():
     try:
         bash_command = "nvidia-smi --query-gpu=index,name,uuid,memory.total,memory.free,memory.used,count,utilization.gpu,utilization.memory --format=csv"
         output = execute_bash_command(bash_command).decode()
-        print(output)
         if "NVIDIA A100" in output:
             print(True)
             print(sys.executable)
             install_and_import("torch", "1.11.0+cu113", "-f", "https://download.pytorch.org/whl/torch_stable.html")
             install_and_import("torchvision", "0.12.0+cu113", "-f",
                                "https://download.pytorch.org/whl/torch_stable.html")
-
     except OSError:
         logging.info("GPU device is not available")
 
