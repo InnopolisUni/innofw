b3d09df1d5366160e0467504f74e53262418de6b
ibragimmergaliev <i.mergaliev@innopolis.ru>
2022-11-11 16:08:52 +0300

    Fixed seqmemloading error

--- examples/sequential_mem_loading.sh
+++ examples/sequential_mem_loading.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 PWD=$(pwd)
-if [ "${PWD##*/}"="examples" ]; then
+PWD=$(pwd)
+
+current_dir=${PWD##*/}
+
+var2="examples"
+if [ "$current_dir" = "$var2" ]; then
   cd ..
 fi
 nohup python train.py experiments=IM_190722_vwer3f23_oneshotlearning.yaml  &
