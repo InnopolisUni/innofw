4c226daaffe0c2f1a013f5e79fad38a74956fa54
Shamil Khastiev <s.hastiev@innopolis.university>
2022-11-11 14:23:46 +0300

    delte script

--- examples/infer_unet.sh
+++
@@ -1,5 +0,0 @@
-# variables
-experiment_name=SK_210722_vfdkmls3_deeplabv3
-weight_path=/mnt/localssd/qazybek/InnoFramework/logs/train/segmentation/SK_210722_vfdkmls3_deeplabv3.yaml/20220805-093357/lightning_logs/version_0/checkpoints/'epoch=1-step=756.ckpt'
-# command
-#python infer.py experiments=$experiment_name +data=$data_path +ckpt_path=$weight_path
