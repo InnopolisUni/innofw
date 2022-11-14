778a9943a6da93e647de50624b725444b141c607
ibragimmergaliev <i.mergaliev@innopolis.ru>
2022-11-14 17:26:29 +0300

    Fix stream.sh to work silently

--- examples/stream.sh
+++ examples/stream.sh
@@ -1,6 +1,6 @@
-pip uninstall opencv-python
-pip uninstall opencv-python-headless
-sudo apt install libgtk2.0-dev pkg-config cmake
-pip install opencv-python==3.4.18.65
+yes | pip uninstall opencv-python
+yes | pip uninstall opencv-python-headless
+sudo apt install libgtk2.0-dev pkg-config cmake -y
+yes | pip install opencv-python==3.4.18.65
 export CAMERA_URI="rtsp://${CAMERA_IP}/live.sdp"
 python innofw/onvif_util/stream.py --uri $CAMERA_URI
