cccb18c5946f97df4bbe095e71d42928a34c7620
Kazybek Askarbek <k.askarbek@innopolis.university>
2022-11-16 09:31:49 +0300

    Update infer_linear_regression.bat

--- examples/infer_linear_regression.bat
+++ examples/infer_linear_regression.bat
@@ -1,3 +1,1 @@
-#experiment_name=KA_130722_9f7134db_linear_regression
-#ckpt_path=https://api.blackhole.ai.innopolis.university/pretrained/house_prices_lin_reg.pickle
 python infer.py experiments=KA_130722_9f7134db_linear_regression +ckpt_path=https://api.blackhole.ai.innopolis.university/pretrained/house_prices_lin_reg.pickle
