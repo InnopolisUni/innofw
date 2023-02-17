a7bd2cecf22fe256c27599d60a9a79a736f5ad00
Kazybek Askarbek <k.askarbek@innopolis.university>
2023-02-17 11:00:30 +0000

    refactored the function

--- innofw/utils/config.py
+++ innofw/utils/config.py
@@ -1,3 +1,5 @@
+from multiprocessing.sharedctypes import Value
+from typing import Optional
 from pydantic import validate_arguments, FilePath
 import hydra
 import yaml
@@ -5,12 +7,14 @@ from hydra import compose, initialize
 
 
 @validate_arguments
-def read_cfg(cfg_path: FilePath, with_overriding=False):
-    if with_overriding:
-        initialize(config_path=str('../config'), job_name="test_app")
-        cfg = compose(config_name="entry.yaml", overrides=[f"experiments={cfg_path.name}",])
-        return cfg
-    else:
+def read_cfg(cfg_path: Optional[FilePath]=None, cfg_name="train.yaml", overrides=None):
+    if overrides is None and cfg_path is not None:
         with open(cfg_path, "r") as f:
             cfg = yaml.safe_load(f)
             return hydra.utils.instantiate(cfg)  # todo: why this returns an object?
+    elif overrides is not None and cfg_path is None:
+        initialize(config_path=str('../../config'), job_name="test_app")
+        cfg = compose(config_name=cfg_name, overrides=overrides)
+        return cfg
+    else:
+        raise ValueError("wrong set of arguments is provided")
