ec526441ce06ca7ea172ec51d2343d68852e6b70
Kazybek Askarbek <k.askarbek@innopolis.university>
2023-02-20 09:17:38 +0000

    updated test cases

--- tests/unit/dataset/semantic_segmentation/test_tiff.py
+++ tests/unit/dataset/semantic_segmentation/test_tiff.py
@@ -1,62 +1,84 @@
+import pytest
+import numpy as np
+from numpy import ndarray
+from hydra.utils import instantiate
+from hydra.errors import InstantiationException
+
+from innofw.constants import SegDataKeys
 from innofw.core.datasets.semantic_segmentation.tiff_dataset import SegmentationDataset
 
-
-# for now just create a folder with segmentation dataset
-# later I will upload it to the minio
-
-# I should have a config file for the dataset
-
-# dataset should contain 4 images and 4 masks
-
-# todo: add full iteration over the dataset
-
-def test_read(images, masks, transform, channels, with_caching):
-    ds = SegmentationDataset(
-        images,
-        masks,
-        transform,
-        channels,
-        with_caching,
-    )
+from tests.fixtures.config.datasets_2 import roads_tiff_dataset_w_masks
+from tests.fixtures.config.augmentations import bare_aug as resize_augmentation
 
 
-def test_with_caching():
-    pass
+@pytest.mark.parametrize(
+    ["cfg", "w_mask", "size", "n_channels"],
+    [
+        [roads_tiff_dataset_w_masks.copy(), True, 2048, 3],
+        [roads_tiff_dataset_w_masks.copy(), False, 2048, 3],
+    ],
+)
+def test_read(cfg, w_mask, size, n_channels):
+    if not w_mask:
+        cfg["masks"] = None
+
+    ds = instantiate(cfg, _convert_="partial")
+    assert ds is not None
+    assert len(ds) > 0
+
+    item = ds[0]
+    assert item is not None
+
+    for item in ds:
+        assert isinstance(item[SegDataKeys.image], ndarray)
+        assert item[SegDataKeys.image].shape == (n_channels, size, size)
+
+        if w_mask:
+            assert isinstance(item[SegDataKeys.label], ndarray)
+            assert item[SegDataKeys.label].shape == (1, size, size)
+
+            # as it is a binary segmentation data
+            # it should have at most two distinct values
+            assert len(np.unique(item[SegDataKeys.label])) <= 2
+            # max value is 1
+            assert item[SegDataKeys.label].max() <= 1
+            # min value is 0
+            assert item[SegDataKeys.label].min() == 0
+        else:
+            with pytest.raises(KeyError):
+                assert item[SegDataKeys.label]
 
 
-def test_with_transform():
-    pass
+@pytest.mark.parametrize(
+    ["ds_cfg", "aug_cfg"], [[roads_tiff_dataset_w_masks.copy(), resize_augmentation.copy()]]
+)
+def test_ds_w_transform(ds_cfg, aug_cfg):
+    ds_cfg["transform"] = aug_cfg
+    test_read(ds_cfg, False, 244, 3)  # resize should decrease the size
 
 
-def test_with_transform_n_caching():
-    pass
-
-
-def test_with_mask():
-    pass
-
-
-def test_with_mask_n_caching():
-    pass
-
-
-def test_with_mask_n_transform():
-    pass
-
-
-def test_with_mask_n_transform_n_caching():
-    pass
-
-
-def test_wrong_img_mask_number():
-    pass
-
-
-def test_wrong_img():
-    pass
-
-
-def test_channels():
+@pytest.mark.parametrize(
+    ["ds_cfg"], [[roads_tiff_dataset_w_masks.copy()]]
+)
+def test_channels(ds_cfg):
     # data should contain 4 channels
     # but 3 needed
-    pass
+    ds_cfg['channels'] = 2
+    test_read(ds_cfg, False, 2048, 2)
+
+
+@pytest.mark.parametrize(["cfg"], [[roads_tiff_dataset_w_masks.copy()]])
+def test_ds_w_caching(cfg):
+    cfg["w_caching"] = True
+    # with masks
+    test_read(cfg, True, 2048, 3)
+    # without masks
+    test_read(cfg, False, 2048, 3)
+
+
+@pytest.mark.parametrize(["cfg"], [[roads_tiff_dataset_w_masks.copy()]])
+def test_wrong_img_mask_number(cfg):
+    cfg['images'] = cfg['images'][:1]
+
+    with pytest.raises(InstantiationException):
+        ds = instantiate(cfg, _convert_="partial")
