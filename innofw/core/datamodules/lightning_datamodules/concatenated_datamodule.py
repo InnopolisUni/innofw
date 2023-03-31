04d055f4cff63572aea230363c3a0a5d0f50a43c
paNikitin <115797306+paNikitin@users.noreply.github.com>
2023-03-31 10:50:06 +0300

    Update concatenated_datamodule.py

--- innofw/core/datamodules/lightning_datamodules/concatenated_datamodule.py
+++ innofw/core/datamodules/lightning_datamodules/concatenated_datamodule.py
@@ -11,6 +11,8 @@ import os.path
 import pathlib
 import pandas as pd
 
+def collate_fn(batch):
+    return tuple(zip(*batch))
 class ConcatenatedLightningDatamodule(BaseLightningDataModule):
 
     framework = [Frameworks.torch]
@@ -39,14 +41,11 @@ class ConcatenatedLightningDatamodule(BaseLightningDataModule):
 
     def setup_train_test_val(self, **kwargs):
         [dm.setup_train_test_val() for dm in self.datamodules]
+        #self.save_preds = self.datamodules[0].save_preds
         self.train_ds = ConcatDataset([dm.train_dataset for dm in self.datamodules])
         self.test_ds = ConcatDataset([dm.test_dataset for dm in self.datamodules])
         self.val_ds = ConcatDataset([dm.val_dataset for dm in self.datamodules])
-        # self.train_ds = self.datamodules[0].train_dataset
-        # self.test_ds = self.datamodules[0].train_dataset
-        # self.val_ds = self.datamodules[0].train_dataset
-        print(type(self.train_ds),'sdasdsadSDDSADSA')
-        
+
     def stage_dataloader(self, dataset, stage):
         if stage in ["val", "test", "predict"]:
             shuffle = False
@@ -54,27 +53,37 @@ class ConcatenatedLightningDatamodule(BaseLightningDataModule):
         else:
             shuffle = self.shuffle
             drop_last = True
+        # detection_coco
+        return DataLoader(
+            dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            collate_fn=collate_fn,
+        )
 
         # sampler = self.samplers[stage]
 
         # if sampler is not None:
         #     shuffle = False
-
-        return DataLoader(
-            dataset,
-            batch_size=self.batch_size,
-            shuffle=shuffle,
-            #sampler=sampler,
-            batch_sampler=None,
-            num_workers=self.num_workers,
-            collate_fn=None,
-            pin_memory=False,  # True
-            drop_last=drop_last,
-            timeout=0,  # pin_memory=False
-            worker_init_fn=None,
-            prefetch_factor=2,
-            persistent_workers=False,
-        )
+        
+        # image_folder_dm
+        # return DataLoader(
+        #     dataset,
+        #     batch_size=self.batch_size,
+        #     shuffle=shuffle,
+        #     #sampler=sampler,
+        #     batch_sampler=None,
+        #     num_workers=self.num_workers,
+        #     collate_fn=None,
+        #     pin_memory=False,  # True
+        #     drop_last=drop_last,
+        #     timeout=0,  # pin_memory=False
+        #     worker_init_fn=None,
+        #     prefetch_factor=2,
+        #     persistent_workers=False,
+        # )
+        
+        
 
     def train_dataloader(self):
         return self.stage_dataloader(self.train_ds, "train")
@@ -89,11 +98,12 @@ class ConcatenatedLightningDatamodule(BaseLightningDataModule):
         return self.stage_dataloader(self.predict_source, "predict")
     
     def save_preds(self, preds, stage: Stages, dst_path: pathlib.Path):
-        out = []
-        for sublist in preds:
-            out.extend(sublist.tolist())
-        images = self.predict_source.image_names
-        df = pd.DataFrame(list(zip(images, out)), columns=["Image name", "Class"])
-        dst_filepath = os.path.join(dst_path, "classification.csv")
-        df.to_csv(dst_filepath)
-        logging.info(f"Saved results to: {dst_filepath}")
+        pass
+        # out = []
+        # for sublist in preds:
+        #     out.extend(sublist.tolist())
+        # images = self.predict_source.image_names
+        # df = pd.DataFrame(list(zip(images, out)), columns=["Image name", "Class"])
+        # dst_filepath = os.path.join(dst_path, "classification.csv")
+        # df.to_csv(dst_filepath)
+        # logging.info(f"Saved results to: {dst_filepath}")
