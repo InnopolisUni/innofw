500ad2489181c9dce39b63ebfa9be73475cdd36f
paNikitin <115797306+paNikitin@users.noreply.github.com>
2023-04-07 22:38:51 +0300

    Update concatenated_datamodule.py

--- innofw/core/datamodules/lightning_datamodules/concatenated_datamodule.py
+++ innofw/core/datamodules/lightning_datamodules/concatenated_datamodule.py
@@ -9,10 +9,15 @@ from innofw.core.augmentations import Augmentation
 import logging
 import os.path
 import pathlib
+import datasets
 import pandas as pd
+from innofw.core.datamodules.lightning_datamodules.drugprot import DataCollatorWithPaddingAndTruncation
+from torch.utils.data import WeightedRandomSampler
+import numpy as np
 
 def collate_fn(batch):
     return tuple(zip(*batch))
+
 class ConcatenatedLightningDatamodule(BaseLightningDataModule):
 
     framework = [Frameworks.torch]
@@ -20,13 +25,17 @@ class ConcatenatedLightningDatamodule(BaseLightningDataModule):
     @validate_arguments
     def __init__(
         self,
+        class_name,
         datamodules,
-        batch_size: int = 16,
+        batch_size: int = 4,
         val_size: float = 0.2,
         num_workers: int = 1,
         augmentations=None,
         infer=None,
         stage=None,
+        channels_num = None,
+        random_seed = None,
+        w_sampler = False,
         *args,
         **kwargs
     ):
@@ -38,52 +47,63 @@ class ConcatenatedLightningDatamodule(BaseLightningDataModule):
         self.infer = infer
         self.val_size = val_size
         self.augmentations=augmentations
-
+        self.class_name = class_name
+        self.channels_num = channels_num
+        self.val_size = val_size
+        self.random_seed = random_seed
+        self.w_sampler = w_sampler
+        self.mul = 1
+        self.batch_sampler = None
+        self.collate_fn = None
+        self.pin_memory = False
+        self.worker_init_fn = None
+        self.prefetch_factor = 1
+        self.persistent_workers = False
+        self.timeout = 0
+        self.shuffle = False
+        self.drop_last = False
+        
     def setup_train_test_val(self, **kwargs):
         [dm.setup_train_test_val() for dm in self.datamodules]
-        #self.save_preds = self.datamodules[0].save_preds
+        
         self.train_ds = ConcatDataset([dm.train_dataset for dm in self.datamodules])
-        self.test_ds = ConcatDataset([dm.test_dataset for dm in self.datamodules])
+        #self.test_ds = ConcatDataset([dm.test_dataset for dm in self.datamodules])
         self.val_ds = ConcatDataset([dm.val_dataset for dm in self.datamodules])
 
     def stage_dataloader(self, dataset, stage):
         if stage in ["val", "test", "predict"]:
-            shuffle = False
-            drop_last = False
+            self.shuffle = False
+            self.drop_last = False
         else:
-            shuffle = self.shuffle
-            drop_last = True
-        # detection_coco
+            self.shuffle = self.shuffle
+            self.drop_last = True
+            
+        if self.class_name == "CocoLightningDataModule":
+           self.collate_fn = collate_fn
+        elif self.class_name == "DrugprotDataModule":
+            self.collate_fn = DataCollatorWithPaddingAndTruncation(
+            max_length=512,
+            sequence_keys=["input_ids", "labels"]
+        )   
+            dataset = self.dataset.with_format("pt")
+        elif self.class_name == "ImageLightningDataModule":
+            self.prefetch_factor = 2
+        
         return DataLoader(
             dataset,
             batch_size=self.batch_size,
+            shuffle=self.shuffle,
+            drop_last=self.drop_last,
             num_workers=self.num_workers,
-            collate_fn=collate_fn,
-        )
-
-        # sampler = self.samplers[stage]
-
-        # if sampler is not None:
-        #     shuffle = False
-        
-        # image_folder_dm
-        # return DataLoader(
-        #     dataset,
-        #     batch_size=self.batch_size,
-        #     shuffle=shuffle,
-        #     #sampler=sampler,
-        #     batch_sampler=None,
-        #     num_workers=self.num_workers,
-        #     collate_fn=None,
-        #     pin_memory=False,  # True
-        #     drop_last=drop_last,
-        #     timeout=0,  # pin_memory=False
-        #     worker_init_fn=None,
-        #     prefetch_factor=2,
-        #     persistent_workers=False,
-        # )
-        
-        
+            collate_fn=self.collate_fn,
+            pin_memory=self.pin_memory,
+            timeout=self.timeout,
+            worker_init_fn=self.worker_init_fn,
+            prefetch_factor=self.prefetch_factor,
+            persistent_workers=self.persistent_workers,
+            #sampler=sampler,
+            #batch_sampler=self.batch_sampler,
+        )       
 
     def train_dataloader(self):
         return self.stage_dataloader(self.train_ds, "train")
@@ -99,11 +119,3 @@ class ConcatenatedLightningDatamodule(BaseLightningDataModule):
     
     def save_preds(self, preds, stage: Stages, dst_path: pathlib.Path):
         pass
-        # out = []
-        # for sublist in preds:
-        #     out.extend(sublist.tolist())
-        # images = self.predict_source.image_names
-        # df = pd.DataFrame(list(zip(images, out)), columns=["Image name", "Class"])
-        # dst_filepath = os.path.join(dst_path, "classification.csv")
-        # df.to_csv(dst_filepath)
-        # logging.info(f"Saved results to: {dst_filepath}")
