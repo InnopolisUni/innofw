b2bb14f6da0e6d9534733223d5729642320132e2
Dnau15 <15dnau@gmail.com>
2023-04-07 16:09:10 +0300

    I have grouped files

--- examples/infer_biobert.sh
+++ examples/infer_biobert.sh
@@ -1,1 +1,1 @@
-python infer.py experiments=regression/GR_300822_LIj29Hw_biobert_ner_drugprot ckpt_path=https://api.blackhole.ai.innopolis.university/pretrained/biobert_ner_drugprot.ckpt
+python infer.py experiments=classification/GR_300822_LIj29Hw_biobert_ner_drugprot ckpt_path=https://api.blackhole.ai.innopolis.university/pretrained/biobert_ner_drugprot.ckpt
