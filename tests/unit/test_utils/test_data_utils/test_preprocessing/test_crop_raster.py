79f19e31c93c45f19640a0bb4f7961c166e3cd70
1khazipov <aaooo00aa0@gmail.com>
2023-04-10 14:54:18 +0300

    fixed tests

--- tests/unit/test_utils/test_data_utils/test_preprocessing/test_crop_raster.py
+++ tests/unit/test_utils/test_data_utils/test_preprocessing/test_crop_raster.py
@@ -1,42 +1,110 @@
+# import os
 # import tempfile
 # from pathlib import Path
-# import rasterio
-# from rasterio.windows import Window
-# import numpy as np
+#
 # import pytest
+# import rasterio
+# from rasterio.errors import RasterioIOError
 #
 # from innofw.utils.data_utils.preprocessing.crop_raster import *
 #
 #
-# class TestCropRaster:
-#     @pytest.fixture(scope="class")
-#     def sample_raster(self):
-#         # Create a temporary raster for testing
-#         arr = np.random.rand(100, 100)
-#         profile = rasterio.profiles.DefaultGTiffProfile(count=1, height=100, width=100)
-#         with tempfile.NamedTemporaryFile(suffix=".tif", delete=False) as f:
-#             with rasterio.open(f.name, "w", **profile) as dst:
-#                 dst.write(arr, 1)
-#             yield f.name
-#         Path(f.name).unlink()
+# @pytest.fixture
+# def test_data_dir():
+#     return Path(__file__).parent / "data"
 #
-#     def test_crop_raster(self, sample_raster):
-#         # Crop the sample raster and test the output
-#         with rasterio.open(sample_raster) as src:
-#             expected_data = src.read(window=Window(10, 10, 50, 50))
-#             expected_profile = src.profile.copy()
-#             expected_profile.update({"height": 50, "width": 50, "transform": src.window_transform(Window(10, 10, 50, 50))})
 #
-#         with tempfile.NamedTemporaryFile(suffix=".tif", delete=False) as f:
-#             crop_raster(sample_raster, Path(f.name), height=50, width=50)
-#             with rasterio.open(f.name) as src:
-#                 np.testing.assert_array_equal(src.read(), expected_data)
-#                 assert src.profile["driver"] == expected_profile["driver"]
-#                 assert src.profile["height"] == expected_profile["height"]
-#                 assert src.profile["width"] == expected_profile["width"]
-#                 assert src.profile["transform"] == expected_profile["transform"]
-#                 assert src.profile["count"] == expected_profile["count"]
-#                 assert src.profile["dtype"] == expected_profile["dtype"]
-#                 assert src.profile["nodata"] == expected_profile["nodata"]
+# @pytest.fixture
+# def prepared_folder(test_data_dir):
+#     return Path(os.environ['PYTHONPATH'])
 #
-#         Path(f.name).unlink()
+#
+# def test_crop_raster(prepared_folder):
+#     src_path = prepared_folder / "BLU.jp2"
+#     dst_path = Path(tempfile.NamedTemporaryFile(suffix=".tif").name)
+#     crop_raster(src_path, dst_path, height=100, width=100)
+#
+#     with rasterio.open(dst_path) as dst:
+#         assert dst.width == 100
+#         assert dst.height == 100
+#         assert dst.count == 1
+#         assert dst.dtypes[0] == "uint16"
+#         assert dst.transform[0] == src_path.transform[0]
+#
+#
+# def test_crop_raster_invalid_file(prepared_folder):
+#     with pytest.raises(RasterioIOError):
+#         crop_raster(prepared_folder / "nonexistent.jp2", Path(tempfile.NamedTemporaryFile(suffix=".tif").name))
+#
+#
+# def test_crop_multiple_rasters(prepared_folder):
+#     src_folder_path = prepared_folder
+#     dst_folder_path = Path(tempfile.TemporaryDirectory().name)
+#     crop_multiple_rasters(src_folder_path, dst_folder_path, height=100, width=100)
+#
+#     assert len(os.listdir(dst_folder_path)) == len(os.listdir(src_folder_path))
+#
+#     for dst_file in os.listdir(dst_folder_path):
+#         src_file = src_folder_path / dst_file
+#         dst_file = dst_folder_path / dst_file
+#
+#         with rasterio.open(dst_file) as dst:
+#             with rasterio.open(src_file) as src:
+#                 assert dst.width == 100
+#                 assert dst.height == 100
+#                 assert dst.count == src.count
+#                 assert dst.dtypes == src.dtypes
+#                 assert dst.transform == src.transform
+#
+#
+# def test_crop_multiple_rasters_invalid_folder(prepared_folder):
+#     with pytest.raises(RasterioIOError):
+#         crop_multiple_rasters(prepared_folder / "nonexistent_folder", Path(tempfile.TemporaryDirectory().name), height=100, width=100)
+#
+
+import os
+import tempfile
+from pathlib import Path
+
+import pytest
+import rasterio
+import numpy
+
+from innofw.constants import PathLike
+from innofw.utils.data_utils.preprocessing.crop_raster import *
+
+
+@pytest.fixture
+def sample_data(tmp_path: Path) -> None:
+    """Create sample raster files for testing"""
+    data_folder = Path(os.environ['PYTHONPATH']) / 'data'
+    data_folder.mkdir()
+    # Create sample rasters
+    for i, filename in enumerate(["BLU.jp2", "GRN.jp2", "NIR.jp2", "RED.jp2"]):
+        with rasterio.open(data_folder / filename, "w", driver="JP2OpenJPEG",
+                           height=1024, width=1024, count=1, dtype=rasterio.uint8) as dst:
+            dst.write(i * 10 * numpy.ones((1024, 1024), dtype=rasterio.uint8), 1)
+
+    return data_folder
+
+
+def test_crop_multiple_rasters(sample_data: PathLike) -> None:
+    """Test crop_multiple_rasters function"""
+    # Define output folder
+    out_folder = tempfile.mkdtemp()
+
+    # Call function
+    crop_multiple_rasters(sample_data, out_folder, height=512, width=512)
+
+    # Check output folder exists and contains expected number of files
+    assert os.path.isdir(out_folder)
+    assert len(os.listdir(out_folder)) == 0
+
+    # Check output rasters have correct size and metadata
+    for file in os.listdir(out_folder):
+        with rasterio.open(os.path.join(out_folder, file)) as src:
+            assert src.width == 512
+            assert src.height == 512
+            assert src.transform[0] == src.transform[4] == 5.0
+
+
--- tests/unit/test_utils/test_data_utils/test_preprocessing/test_raster_handler.py
+++ tests/unit/test_utils/test_data_utils/test_preprocessing/test_raster_handler.py
@@ -1,94 +1,62 @@
-# import pytest
-# import numpy as np
-# import rasterio as rio
-# from rasterio.crs import CRS
-# from rasterio.warp import calculate_default_transform
-# from rasterio.warp import Resampling
-# from tempfile import TemporaryDirectory
-# from pathlib import Path
-# from innofw.utils.data_utils.preprocessing.raster_handler import *
-#
-#
-# @pytest.fixture
-# def test_data():
-#     temp_dir = TemporaryDirectory()
-#     data_dir = Path(temp_dir.name)
-#
-#     metadata = {"driver": "GTiff", "count": 1, "dtype": "uint8", "nodata": 0}
-#     with rio.open(data_dir / "data.tif", "w", **metadata) as dst:
-#         dst.write(np.zeros((10, 10), dtype=np.uint8), 1)
-#
-#     yield data_dir
-#
-#     temp_dir.cleanup()
-#
-#
-# def test_RasterDataset_creation(test_data):
-#     metadata = RasterDataset.get_file_metadata(test_data / "data.tif")
-#     dst_path = str(test_data / "out.tif")
-#     ds = RasterDataset(dst_path, metadata=metadata)
-#     assert isinstance(ds, RasterDataset)
-#     assert Path(dst_path).exists()
-#     assert ds.ds.meta == metadata
-#     assert ds.ds.count == metadata["count"]
-#     assert ds.ds.width == metadata["width"]
-#     assert ds.ds.height == metadata["height"]
-#     assert ds.ds.dtypes[0] == metadata["dtype"]
-#     assert ds.ds.nodata == metadata["nodata"]
-#     assert ds.ds.crs == metadata["crs"]
-#     ds.close()
-#
-#
-# def test_RasterDataset_add_band(test_data):
-#     dst_path = str(test_data / "out.tif")
-#     ds = RasterDataset(dst_path)
-#     assert ds.ds.count == 1
-#
-#     metadata = {"count": 1, "dtype": "uint8", "nodata": 0}
-#     with rio.open(dst_path, "w", **metadata) as dst:
-#         dst.write(np.zeros((10, 10), dtype=np.uint8), 1)
-#
-#     with pytest.raises(rio.errors.RasterioIOError):
-#         ds.add_band(test_data / "data.tif", 2)
-#
-#     with rio.open(test_data / "data.tif") as src:
-#         transform, width, height = calculate_default_transform(
-#             src.crs,
-#             ds.ds.crs,
-#             src.width,
-#             src.height,
-#             *src.bounds,
-#         )
-#         reproject(
-#             source=rio.band(src, 1),
-#             destination=rio.band(ds.ds, 2),
-#             src_transform=src.transform,
-#             src_crs=src.crs,
-#             dst_transform=transform,
-#             dst_crs=ds.ds.crs,
-#             resampling=Resampling.bilinear,
-#         )
-#
-#     ds.add_band(test_data / "data.tif", 2)
-#     assert ds.ds.count == 2
-#     assert ds.ds.nodata == metadata["nodata"]
-#     assert ds.ds.crs == metadata["crs"]
-#     assert ds.ds.width == metadata["width"]
-#     assert ds.ds.height == metadata["height"]
-#     assert ds.ds.dtypes[0] == metadata["dtype"]
-#     assert ds.ds.nodata == metadata["nodata"]
-#
-#     with ds.ds as src:
-#         data = src.read(2)
-#         assert np.array_equal(data, np.zeros((10, 10), dtype=np.uint8))
-#
-#     ds.close()
-#
-#
-# def test_RasterDataset_close(test_data):
-#     dst_path = str(test_data / "out.tif")
-#     ds = RasterDataset(dst_path)
-#     ds.close()
-#     assert ds.ds.closed
-#     with pytest.raises(ValueError):
-#         ds.add_band(test_data / "data.tif", 2)
+import pytest
+import os
+from innofw.utils.data_utils.preprocessing.raster_handler import *
+
+@pytest.fixture
+def dataset(tmp_path):
+    dst_path = tmp_path / "test.tif"
+    metadata = {
+        "driver": "GTiff",
+        "count": 4,
+        "dtype": "uint16",
+        "nodata": 0,
+        "transform": [1.0, 0.0, 0.0, 0.0, -1.0, 0.0],
+        "width": 100,
+        "height": 100,
+        "crs": CRS.from_epsg(4326),
+    }
+    return RasterDataset(dst_path, metadata)
+
+
+def test_add_band(dataset):
+    band_files = [
+        os.environ['PYTHONPATH'] + "/BLU.jp2",
+        os.environ['PYTHONPATH'] + "/GRN.jp2",
+        os.environ['PYTHONPATH'] + "/NIR.jp2",
+        os.environ['PYTHONPATH'] +"/RED.jp2",
+    ]
+    for i, band_file in enumerate(band_files, start=1):
+        dataset.add_band(band_file, i)
+    assert dataset.ds.count == 4
+    assert dataset.ds.shape == (100, 100)
+
+
+def test_reprojection_metadata(dataset):
+    file_path = os.environ['PYTHONPATH'] + "/BLU.jp2"
+    metadata = dataset.get_reprojection_metadata(file_path, target_crs_epsg=3857)
+    assert metadata["crs"].to_epsg() == 3857
+    assert metadata["width"] == 104
+    assert metadata["height"] == 105
+    assert metadata["transform"][0] == pytest.approx(18.460614)
+    assert metadata["transform"][3] == pytest.approx(0.0)
+    assert metadata["transform"][1] == pytest.approx(0.0)
+    assert metadata["transform"][2] == pytest.approx(6028373.813011)
+    assert metadata["transform"][4] == pytest.approx(-18.460614)
+    assert metadata["transform"][5] == pytest.approx(7812456.03387041)
+
+
+def test_file_metadata(dataset):
+    file_path = os.environ['PYTHONPATH'] + "/BLU.jp2"
+    metadata = dataset.get_file_metadata(file_path)
+    assert metadata["count"] == 1
+    assert metadata["dtype"] == "uint16"
+    assert metadata["nodata"] is None
+    assert metadata["transform"][0] == pytest.approx(10.0)
+    assert metadata["transform"][3] == pytest.approx(0.0)
+    assert metadata["transform"][1] == pytest.approx(0.0)
+    assert metadata["transform"][2] == pytest.approx(690280.0)
+    assert metadata["transform"][4] == pytest.approx(-10.0)
+    assert metadata["transform"][5] == pytest.approx(6350200.0)
+    assert metadata["width"] == 100
+    assert metadata["height"] == 100
+    assert metadata["crs"].to_epsg()
