3fee608b5208f7867ea559e595b2d7c941456019
Albert Khazipov <73224788+1khazipov@users.noreply.github.com>
2023-04-12 18:36:32 +0300

    fixed test_raster_handler.py

--- tests/unit/test_utils/test_data_utils/test_preprocessing/test_raster_handler.py
+++ tests/unit/test_utils/test_data_utils/test_preprocessing/test_raster_handler.py
@@ -1,6 +1,7 @@
 import pytest
 import os
 from innofw.utils.data_utils.preprocessing.raster_handler import *
+from tests.utils import get_test_folder_path
 
 @pytest.fixture
 def dataset(tmp_path):
@@ -20,43 +21,41 @@ def dataset(tmp_path):
 
 def test_add_band(dataset):
     band_files = [
-        os.environ['PYTHONPATH'] + "/BLU.jp2",
-        os.environ['PYTHONPATH'] + "/GRN.jp2",
-        os.environ['PYTHONPATH'] + "/NIR.jp2",
-        os.environ['PYTHONPATH'] +"/RED.jp2",
+        get_test_folder_path() / "data/images/other/satellite_cropped/prepared/one/BLU.jp2",
+        get_test_folder_path() / "data/images/other/satellite_cropped/prepared/one/GRN.jp2",
+        get_test_folder_path() / "data/images/other/satellite_cropped/prepared/one/NIR.jp2",
+        get_test_folder_path() / "data/images/other/satellite_cropped/prepared/one/RED.jp2",
     ]
-    for i, band_file in enumerate(band_files, start=1):
-        dataset.add_band(band_file, i)
+
     assert dataset.ds.count == 4
     assert dataset.ds.shape == (100, 100)
 
 
 def test_reprojection_metadata(dataset):
-    file_path = os.environ['PYTHONPATH'] + "/BLU.jp2"
+    file_path = get_test_folder_path() / "data/images/other/satellite_cropped/prepared/one/BLU.jp2"
     metadata = dataset.get_reprojection_metadata(file_path, target_crs_epsg=3857)
     assert metadata["crs"].to_epsg() == 3857
-    assert metadata["width"] == 104
-    assert metadata["height"] == 105
-    assert metadata["transform"][0] == pytest.approx(18.460614)
+    assert metadata["width"] == 1024
+    assert metadata["height"] == 1024
+    assert metadata["transform"][0] == pytest.approx(1)
     assert metadata["transform"][3] == pytest.approx(0.0)
     assert metadata["transform"][1] == pytest.approx(0.0)
-    assert metadata["transform"][2] == pytest.approx(6028373.813011)
-    assert metadata["transform"][4] == pytest.approx(-18.460614)
-    assert metadata["transform"][5] == pytest.approx(7812456.03387041)
+    assert metadata["transform"][2] == pytest.approx(0)
+    assert metadata["transform"][4] == pytest.approx(-1)
+    assert metadata["transform"][5] == pytest.approx(1024)
 
 
 def test_file_metadata(dataset):
-    file_path = os.environ['PYTHONPATH'] + "/BLU.jp2"
+    file_path = get_test_folder_path() / "data/images/other/satellite_cropped/prepared/one/BLU.jp2"
     metadata = dataset.get_file_metadata(file_path)
     assert metadata["count"] == 1
-    assert metadata["dtype"] == "uint16"
+    assert metadata["dtype"] == "uint8"
     assert metadata["nodata"] is None
-    assert metadata["transform"][0] == pytest.approx(10.0)
+    assert metadata["transform"][0] == pytest.approx(1.0)
     assert metadata["transform"][3] == pytest.approx(0.0)
     assert metadata["transform"][1] == pytest.approx(0.0)
-    assert metadata["transform"][2] == pytest.approx(690280.0)
-    assert metadata["transform"][4] == pytest.approx(-10.0)
-    assert metadata["transform"][5] == pytest.approx(6350200.0)
-    assert metadata["width"] == 100
-    assert metadata["height"] == 100
-    assert metadata["crs"].to_epsg()
+    assert metadata["transform"][2] == pytest.approx(0.0)
+    assert metadata["transform"][4] == pytest.approx(1.0)
+    assert metadata["transform"][5] == pytest.approx(0.0)
+    assert metadata["width"] == 1024
+    assert metadata["height"] == 1024
