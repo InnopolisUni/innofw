2a6dc920a6cfae5032580d31d11b8923e69afc19
Albert Khazipov <73224788+1khazipov@users.noreply.github.com>
2023-04-17 01:51:48 +0300

    Update test_db_scan.py

--- tests/unit/test_utils/test_data_utils/test_transforms/test_db_scan.py
+++ tests/unit/test_utils/test_data_utils/test_transforms/test_db_scan.py
@@ -5,15 +5,17 @@ from numpy import max as npmax
 from numpy import min as npmin
 from numpy import zeros
 from innofw.utils.data_utils.transforms.db_scan import *
+import pytest
 
 
-def test_make_hist():
-    img = np.array([[0, 0], [0, 0]])
+@pytest.mark.parametrize("img, expected_hist", [
+    (np.array([[0, 0], [0, 0]]), defaultdict(int, {0: 4})),
+    (np.array([[0, 1], [1, 0]]), defaultdict(int, {0: 2, 1: 2})),
+    # Add more test cases as needed
+])
+def test_make_hist(img, expected_hist):
     hist = make_hist(img)
-    assert hist == defaultdict(int, {0: 4})
-
-    hist = make_hist(np.array([[0, 1], [1, 0]]))
-    assert hist == defaultdict(int, {0: 2, 1: 2})
+    assert hist == expected_hist
 
 
 def test_make_kernel_trick():
@@ -28,20 +30,20 @@ def test_make_kernel_trick():
 
 
 def test_dekernel():
-    zipped = [((1, 1, 1), 1), ((1, 1, 1), 2)]
+    zipped = [((1, 2, 3), 1), ((4, 5, 6), 2)]
     dekerneled = dekernel(zipped, shape=(2, 2))
-    assert_array_equal(dekerneled, np.array([[0, 0], [0, 2]]))
+    assert_array_equal(dekerneled, np.array([[0, 0], [0, 0]]))
 
 
 def test_make_mask():
-    img = np.array([[1, 0], [0, 1]])
+    img = np.array([[1, 0, 1], [0, 1, 0], [1, 0, 1]])
     mask = make_mask(img, cluster=1)
-    assert_array_equal(mask, np.array([[1, 0], [0, 1]]))
+    assert_array_equal(mask, np.array([[1, 0, 1], [0, 1, 0], [1, 0, 1]]))
 
-    mask = make_mask(img, cluster=2)
-    assert_array_equal(mask, np.array([[0, 0], [0, 0]]))
+    mask = make_mask(img, cluster=0)
+    assert_array_equal(mask, np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]]))
 
 
 def test_make_contrasted():
-    contrasted = make_contrasted(np.array([[127, 127], [127, 127]]), contrast=50)
-    assert_array_equal(contrasted, np.array([[127, 127], [127, 127]]))
+    contrasted = make_contrasted(np.array([[0, 0], [127, 127]]), contrast=2)
+    assert_array_equal(contrasted, np.array([[-4, -4], [127, 127]]))
