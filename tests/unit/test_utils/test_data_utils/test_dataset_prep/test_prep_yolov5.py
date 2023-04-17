fb733b8c5b20fb4fe3fed52d20d52240535ef09d
Albert Khazipov <73224788+1khazipov@users.noreply.github.com>
2023-04-17 14:59:20 +0300

    Update test_prep_yolov5.py

--- tests/unit/test_utils/test_data_utils/test_dataset_prep/test_prep_yolov5.py
+++ tests/unit/test_utils/test_data_utils/test_dataset_prep/test_prep_yolov5.py
@@ -8,7 +8,7 @@ from innofw.utils.data_utils.dataset_prep.prep_yolov5 import prepare_dataset
 
 
 class TestPrepareDataset:
-    def test_prepare_dataset(self, data_dir, train_names_file, test_names_file, test_dir):
+    def test_prepare_dataset(self, data_dir, train_names_file, test_names_file, temp_dir):
         # Set up destination path
         dst_path = temp_dir / "output"
         dst_path.mkdir()
@@ -29,9 +29,5 @@ class TestPrepareDataset:
             assert (dst_path / d / "labels").is_dir()
 
             # Check that correct number of files were copied
-            assert len(list((dst_path / "train" / "images").glob("*"))) == 5
-            assert len(list((dst_path / "train" / "labels").glob("*"))) == 5
-            assert len(list((dst_path / "test" / "images").glob("*"))) == 5
-            assert len(list((dst_path / "test" / "labels").glob("*"))) == 5
-            assert len(list((dst_path / "infer" / "images").glob("*"))) == 5
-            assert len(list((dst_path / "infer" / "labels").glob("*"))) == 5
+            assert len(list((dst_path / d / "images").glob("*"))) == 5
+            assert len(list((dst_path / d / "labels").glob("*"))) == 5
