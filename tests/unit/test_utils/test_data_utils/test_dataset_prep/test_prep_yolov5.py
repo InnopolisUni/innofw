e92dd93966d823c8a4d646931b10a8ca805b8737
Albert Khazipov <73224788+1khazipov@users.noreply.github.com>
2023-04-17 12:57:05 +0300

    Update test_prep_yolov5.py

--- tests/unit/test_utils/test_data_utils/test_dataset_prep/test_prep_yolov5.py
+++ tests/unit/test_utils/test_data_utils/test_dataset_prep/test_prep_yolov5.py
@@ -7,48 +7,10 @@ import pytest
 from innofw.utils.data_utils.dataset_prep.prep_yolov5 import prepare_dataset
 
 
-@pytest.fixture(scope="module")
-def test_dir():
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        tmp_dir = Path(tmp_dir)
-        yield tmp_dir
-
-
-@pytest.fixture(scope="module")
-def data_dir(test_dir):
-    data_dir = test_dir / "data"
-    data_dir.mkdir()
-    # create dummy image and label files
-    for i in range(10):
-        with open(data_dir / f"{i}.jpeg", "w") as f:
-            f.write("image file")
-        with open(data_dir / f"{i}.txt", "w") as f:
-            f.write("label file")
-    return data_dir
-
-
-@pytest.fixture(scope="module")
-def train_names_file(test_dir):
-    train_names_file = test_dir / "train.txt"
-    with open(train_names_file, "w") as f:
-        for i in range(5):
-            f.write(f"{i}\n")
-    return train_names_file
-
-
-@pytest.fixture(scope="module")
-def test_names_file(test_dir):
-    test_names_file = test_dir / "test.txt"
-    with open(test_names_file, "w") as f:
-        for i in range(5, 10):
-            f.write(f"{i}\n")
-    return test_names_file
-
-
 class TestPrepareDataset:
     def test_prepare_dataset(self, data_dir, train_names_file, test_names_file, test_dir):
         # Set up destination path
-        dst_path = test_dir / "output"
+        dst_path = temp_dir / "output"
         dst_path.mkdir()
 
         # Test prepare_dataset function
