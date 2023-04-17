cfc37fc06a839bd2e0ba62c08dbc1eee444fba57
Albert Khazipov <73224788+1khazipov@users.noreply.github.com>
2023-04-17 14:30:02 +0300

    Update conftest.py

--- tests/unit/test_utils/test_data_utils/test_dataset_prep/conftest.py
+++ tests/unit/test_utils/test_data_utils/test_dataset_prep/conftest.py
@@ -1,38 +1,32 @@
-import shutil
-from pathlib import Path
-
 import pytest
 
-from innofw.utils.data_utils.dataset_prep.prep_yolov5 import prepare_dataset
 
-import tempfile
+@pytest.fixture(scope="module")
+def data_dir(temp_dir):
+    data_dir = temp_dir / "data"
+    data_dir.mkdir()
+    # create dummy image and label files
+    for i in range(10):
+        with open(data_dir / f"{i}.jpeg", "w") as f:
+            f.write("image file")
+        with open(data_dir / f"{i}.txt", "w") as f:
+            f.write("label file")
+    return data_dir
 
 
-class TestPrepareDataset:
-    def test_prepare_dataset(self, data_dir, train_names_file, test_names_file, temp_dir):
-        # Set up destination path
-        dst_path = temp_dir / "output"
-        dst_path.mkdir()
+@pytest.fixture(scope="module")
+def train_names_file(temp_dir):
+    train_names_file = temp_dir / "train.txt"
+    with open(train_names_file, "w") as f:
+        for i in range(5):
+            f.write(f"{i}\n")
+    return train_names_file
 
-        # Test prepare_dataset function
-        prepare_dataset(
-            src_img_path=data_dir,
-            src_label_path=data_dir,
-            train_names_file=train_names_file,
-            test_names_file=test_names_file,
-            dst_path=dst_path,
-        )
 
-        # Check that train, test, and infer directories are created
-        for d in ["train", "test", "infer"]:
-            assert (dst_path / d).is_dir()
-            assert (dst_path / d / "images").is_dir()
-            assert (dst_path / d / "labels").is_dir()
-
-        # Check that correct number of files were copied
-        assert len(list((dst_path / "train" / "images").glob("*"))) == 5
-        assert len(list((dst_path / "train" / "labels").glob("*"))) == 5
-        assert len(list((dst_path / "test" / "images").glob("*"))) == 5
-        assert len(list((dst_path / "test" / "labels").glob("*"))) == 5
-        assert len(list((dst_path / "infer" / "images").glob("*"))) == 5
-        assert len(list((dst_path / "infer" / "labels").glob("*"))) == 5
+@pytest.fixture(scope="module")
+def test_names_file(temp_dir):
+    test_names_file = temp_dir / "test.txt"
+    with open(test_names_file, "w") as f:
+        for i in range(5, 10):
+            f.write(f"{i}\n")
+    return test_names_file
