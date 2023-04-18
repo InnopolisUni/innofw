8481205cf2d7c2416dcee0d2ad27027053375ee4
Albert Khazipov <73224788+1khazipov@users.noreply.github.com>
2023-04-18 13:50:18 +0300

    Update test_crop_raster.py

--- tests/unit/test_utils/test_data_utils/test_preprocessing/test_crop_raster.py
+++ tests/unit/test_utils/test_data_utils/test_preprocessing/test_crop_raster.py
@@ -1,11 +1,9 @@
 import os
 import tempfile
 from pathlib import Path
-
 import pytest
 import rasterio
 import numpy
-
 from innofw.constants import PathLike
 from innofw.utils.data_utils.preprocessing.crop_raster import *
 from tests.utils import get_test_folder_path
@@ -17,15 +15,14 @@ def test_crop_multiple_rasters(sample_data: PathLike) -> None:
     out_folder = tempfile.mkdtemp()
 
     # Call function
-    crop_multiple_rasters(sample_data, out_folder, height=512, width=512)
+    crop_multiple_rasters(sample_data, out_folder, height=512, width=512, extension='.jp2')
 
     # Check output folder exists and contains expected number of files
     assert os.path.isdir(out_folder)
-    assert len(os.listdir(out_folder)) == 0
+    assert len(os.listdir(out_folder)) == 4
 
     # Check output rasters have correct size and metadata
     for file in os.listdir(out_folder):
         with rasterio.open(os.path.join(out_folder, file)) as src:
             assert src.width == 512
             assert src.height == 512
-            assert src.transform[0] == src.transform[4] == 5.0
