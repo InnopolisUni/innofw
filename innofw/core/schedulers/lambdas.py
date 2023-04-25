b9af0fcd0bce61908e59ed0f9a52b6634ca01f3d
Kazybek Askarbek <k.askarbek@innopolis.university>
2023-04-25 06:54:44 +0000

    removed redundant lines

--- innofw/core/schedulers/lambdas.py
+++ innofw/core/schedulers/lambdas.py
@@ -1,13 +1,11 @@
-import torch
-
-
 def dummy_func(epoch):
-    print('here')
-    print(1/ 0 )
+    print("here")
+    print(1 / 0)
     return (epoch * 0.01) + 0.05
+
 
 def func():
     return lambda epoch: epoch // 30
-lambda1 = func
 
-# cfg = DictConfig({"_target_": "torch.optim.lr_scheduler.LambdaLR", "lr_lambda": {"_target_": "innofw.core.schedulers.lambdas.lambda1"}})
+
+lambda1 = func
