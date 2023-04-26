9c477830d18a475f629e350ee3c28154bcd7fd04
MostafaKhaled2017 <m.kira@innopolis.university>
2023-04-26 10:53:26 +0000

    Fixing Bug

--- innofw/core/integrations/ultralytics/datamodule.py
+++ innofw/core/integrations/ultralytics/datamodule.py
@@ -216,7 +216,7 @@ class YOLOV5DataModuleAdapter(BaseDataModule):
         self.data = str(new_data_path / "data.yaml")
 
         self.train_dataset = str(new_img_path / "train")
-        self.val_dataset = str(new_img_path / "test")
+        self.val_dataset = str(new_img_path / "val")
         self.test_dataset = str(new_img_path / "test")
         # create a yaml file
         with open(self.data, "w+") as file:
