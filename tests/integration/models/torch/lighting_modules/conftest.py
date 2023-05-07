68ba767a837ccc2e3c3079f302c3ebfef5b0858e
Kamil Sabbagh <kamil.mousa9@gmail.com>
2023-05-07 13:55:51 +0300

    Adding fitted model fixture and tempdic

--- tests/integration/models/torch/lighting_modules/conftest.py
+++ tests/integration/models/torch/lighting_modules/conftest.py
@@ -1,8 +1,16 @@
+import tempfile
+
 import pytest
+import torch
 from omegaconf import DictConfig
+from pytorch_lightning import LightningDataModule
 from pytorch_lightning import LightningModule
+from pytorch_lightning import Trainer
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 
 from innofw.constants import Frameworks
+from innofw.constants import SegDataKeys
 from innofw.core.models.torch.lightning_modules.segmentation import (
     SemanticSegmentationLightningModule,
 )
@@ -12,6 +20,38 @@ from tests.fixtures.config import losses as fixt_losses
 from tests.fixtures.config import models as fixt_models
 from tests.fixtures.config import optimizers as fixt_optimizers
 from tests.fixtures.config import trainers as fixt_trainers
+
+
+class DummyDataset(Dataset):
+    def __init__(self, num_samples):
+        self.num_samples = num_samples
+
+    def __getitem__(self, index):
+        x = torch.rand(3, 224, 224)
+        y = torch.randint(0, 2, (1, 224, 224))
+        return {SegDataKeys.image: x, SegDataKeys.label: y}
+
+    def __len__(self):
+        return self.num_samples
+
+
+class DummyDataModule(LightningDataModule):
+    def __init__(self, num_samples: int, batch_size: int = 4):
+        super().__init__()
+        self.num_samples = num_samples
+        self.batch_size = batch_size
+
+    def setup(self, stage=None):
+        self.dataset = DummyDataset(self.num_samples)
+
+    def train_dataloader(self):
+        return DataLoader(self.dataset, batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.dataset, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.dataset, batch_size=self.batch_size)
 
 
 @pytest.fixture
@@ -32,3 +72,35 @@ def segmentation_module() -> LightningModule:
     )
 
     return module
+
+
+@pytest.fixture
+def trainer_with_temporary_directory():
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        trainer = Trainer(
+            max_epochs=2,
+            devices=1,
+            accelerator="gpu",
+            default_root_dir=tmp_dir,
+        )
+        yield trainer, tmp_dir
+
+
+@pytest.fixture
+def dummy_data_module():
+    return DummyDataModule(num_samples=100, batch_size=4)
+
+
+@pytest.fixture(scope="module")
+def fitted_segmentation_module(
+    segmentation_module: LightningModule,
+    trainer_with_temporary_directory,
+    dummy_data_module: DummyDataModule,
+):
+    trainer, _ = trainer_with_temporary_directory
+    dataloader = dummy_data_module.train_dataloader()
+    trainer.fit(segmentation_module, train_dataloaders=dataloader)
+    segmentation_module.trainer = (
+        trainer  # Add the trainer to the fitted module for future use
+    )
+    return segmentation_module
--- tests/integration/models/torch/lighting_modules/test_segmentation.py
+++ tests/integration/models/torch/lighting_modules/test_segmentation.py
@@ -1,60 +1,24 @@
-import os
-import shutil
-
 import pytest
 import torch
-from pytorch_lightning import LightningDataModule
-from pytorch_lightning import LightningModule
-from pytorch_lightning import Trainer
-from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
-
-from innofw.constants import SegDataKeys
 
 
-class DummyDataset(Dataset):
-    def __init__(self, num_samples):
-        self.num_samples = num_samples
+def test_training_with_checkpoint(
+    fitted_segmentation_module,
+    trainer_with_temporary_directory,
+    dummy_data_module,
+):
+    trainer, checkpoint_dir = trainer_with_temporary_directory
+    dataloader = dummy_data_module.train_dataloader()
 
-    def __getitem__(self, index):
-        x = torch.rand(3, 224, 224)
-        y = torch.randint(0, 2, (1, 224, 224))
-        return {SegDataKeys.image: x, SegDataKeys.label: y}
+    # First training phase is already done in the fitted_segmentation_module fixture
+    last_checkpoint_path = (
+        fitted_segmentation_module.trainer.checkpoint_callback.best_model_path
+    )
 
-    def __len__(self):
-        return self.num_samples
-
-
-class DummyDataModule(LightningDataModule):
-    def __init__(self, num_samples: int, batch_size: int = 4):
-        super().__init__()
-        self.num_samples = num_samples
-        self.batch_size = batch_size
-
-    def setup(self, stage=None):
-        self.dataset = DummyDataset(self.num_samples)
-
-    def train_dataloader(self):
-        return DataLoader(self.dataset, batch_size=self.batch_size)
-
-    def val_dataloader(self):
-        return DataLoader(self.dataset, batch_size=self.batch_size)
-
-    def test_dataloader(self):
-        return DataLoader(self.dataset, batch_size=self.batch_size)
-
-
-def test_training_with_checkpoint(segmentation_module: LightningModule):
-    checkpoint_dir = "checkpoints"
-    os.makedirs(checkpoint_dir, exist_ok=True)
-
-    trainer = Trainer(max_epochs=2, devices=1, default_root_dir=checkpoint_dir)
-    dataset = DummyDataset(num_samples=100)
-    dataloader = DataLoader(dataset, batch_size=4)
-    trainer.fit(segmentation_module, train_dataloaders=dataloader)
+    # Continue training using the fitted_segmentation_module
     trainer.fit(
-        segmentation_module,
-        ckpt_path="checkpoints/lightning_logs/version_0/checkpoints/epoch=1-step=50.ckpt",
+        fitted_segmentation_module,
+        ckpt_path=last_checkpoint_path,
         train_dataloaders=dataloader,
     )
 
@@ -62,30 +26,22 @@ def test_training_with_checkpoint(segmentation_module: LightningModule):
 @pytest.mark.skipif(
     not torch.cuda.is_available(), reason="No GPU is found on this machine"
 )
-def test_training_without_checkpoint(segmentation_module: LightningModule):
-    trainer = Trainer(accelerator="gpu", max_epochs=1, devices=1)
-    dataset = DummyDataset(num_samples=100)
-    dataloader = DataLoader(dataset, batch_size=4)
+def test_training_without_checkpoint_with_gpu(
+    segmentation_module, trainer_with_temporary_directory, dummy_data_module
+):
+    trainer, checkpoint_dir = trainer_with_temporary_directory
+    dataloader = dummy_data_module.train_dataloader()
     trainer.fit(segmentation_module, train_dataloaders=dataloader)
 
-    training_metric_values = segmentation_module.training_metric_values
 
-    # Choose a metric to check for improvement, e.g., train_f1
-    metric_name = "BinaryF1Score"
-    print(training_metric_values)
-    # Check if the chosen metric is improving
-    for i in range(1, len(training_metric_values)):
-        assert (
-            training_metric_values[i][metric_name]
-            > training_metric_values[i - 1][metric_name]
-        )
-
-
-def test_testing_without_checkpoint(segmentation_module: LightningModule):
-    trainer = Trainer(max_epochs=1, accelerator="gpu", devices=1)
-    dataset = DummyDataset(num_samples=100)
-    dataloader = DataLoader(dataset, batch_size=4)
-    test_results = trainer.test(segmentation_module, dataloaders=dataloader)
+def test_testing_without_checkpoint(
+    fitted_segmentation_module, dummy_data_module
+):
+    trainer = fitted_segmentation_module.trainer
+    dataloader = dummy_data_module.test_dataloader()
+    test_results = trainer.test(
+        fitted_segmentation_module, dataloaders=dataloader
+    )
 
     # Check if test_results is a non-empty list
     assert len(test_results) > 0
@@ -97,63 +53,50 @@ def test_testing_without_checkpoint(segmentation_module: LightningModule):
         "test_BinaryPrecision",
         "test_BinaryRecall",
     }
-    for metric in expected_metrics:
-        assert metric in test_results[0]
+
+    training_metric_values = fitted_segmentation_module.training_metric_values
+    metric_name = "BinaryF1Score"
+
+    for i in range(1, len(training_metric_values)):
+        assert (
+            training_metric_values[i][metric_name]
+            > training_metric_values[i - 1][metric_name]
+        )
 
 
-def test_predicting_without_checkpoint(segmentation_module: LightningModule):
-    trainer = Trainer(max_epochs=1, devices=1)
-    predict_dataloader = DataLoader(DummyDataset(10), batch_size=4)
-    # predictions = predict(segmentation_module, predict_dataloader)
-    trainer.predict(segmentation_module, dataloaders=predict_dataloader)
+def test_predicting_without_checkpoint(
+    fitted_segmentation_module, dummy_data_module
+):
+    trainer = fitted_segmentation_module.trainer
+    dataloader = dummy_data_module.train_dataloader()
+    trainer.predict(fitted_segmentation_module, dataloaders=dataloader)
 
 
-def test_testing_with_checkpoint(segmentation_module: LightningModule):
-    checkpoint_path = "checkpoints"
-    os.makedirs(checkpoint_path, exist_ok=True)
-
-    # # Set up the trainer
-    # checkpoint_callback = ModelCheckpoint(dirpath=checkpoint_path, save_top_k=-1)
-    trainer = Trainer(
-        max_epochs=2, devices=1, default_root_dir=checkpoint_path
-    )
-
-    dataset = DummyDataset(num_samples=100)
-    dataloader = DataLoader(dataset, batch_size=4)
-
-    # Fit the model
-    trainer.fit(segmentation_module, dataloader)
+def test_testing_with_checkpoint(
+    fitted_segmentation_module, dummy_data_module
+):
+    trainer = fitted_segmentation_module.trainer
+    dataloader = dummy_data_module.train_dataloader()
+    last_checkpoint_path = trainer.checkpoint_callback.best_model_path
 
     # Test the loaded model
     trainer.test(
-        segmentation_module,
-        ckpt_path="checkpoints/lightning_logs/version_0/checkpoints/epoch=1-step=50.ckpt",
+        fitted_segmentation_module,
+        ckpt_path=last_checkpoint_path,
         dataloaders=dataloader,
     )
 
-    # Delete the checkpoint folder
-    shutil.rmtree(checkpoint_path)
 
-
-def test_predicting_with_checkpoint(segmentation_module: LightningModule):
-    checkpoint_path = "checkpoints"
-
-    # Instantiate the DummyDataModule
-    data_module = DummyDataModule(num_samples=100, batch_size=4)
-
-    # Instantiate the Trainer with desired configurations
-    trainer = Trainer(
-        max_epochs=2, devices=1, default_root_dir=checkpoint_path
-    )
-
-    # Fit the model (train and validate)
-    trainer.fit(segmentation_module, data_module)
+def test_predicting_with_checkpoint(
+    fitted_segmentation_module, dummy_data_module
+):
+    trainer = fitted_segmentation_module.trainer
+    dataloader = dummy_data_module.train_dataloader()
+    last_checkpoint_path = trainer.checkpoint_callback.best_model_path
 
     # Create a DataLoader for the prediction data
-    predict_dataloader = DataLoader(DummyDataset(10), batch_size=4)
-
     trainer.predict(
-        segmentation_module,
-        ckpt_path="checkpoints/lightning_logs/version_0/checkpoints/epoch=1-step=50.ckpt",
-        dataloaders=predict_dataloader,
+        fitted_segmentation_module,
+        ckpt_path=last_checkpoint_path,
+        dataloaders=dataloader,
     )
