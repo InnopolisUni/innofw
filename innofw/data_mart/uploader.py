e7513bc404b0b9069c77c8fda59251f64385d2d1
sadjava <110330732+sadjava@users.noreply.github.com>
2023-05-10 16:40:39 +0300

    Refactored upload functions (#175)
    * updated upload functions
    
    * refactored upload functions
    
    * fixed typos
    
    * fixed typos
    
    * fixed typos
    
    * fixed typos
    
    * set rule for configs to be in config directory
    
    * fixed typos
    
    * fixed typo
    
    ---------
    
    Co-authored-by: Kazybek Askarbek <k.askarbek@innopolis.university>

--- innofw/data_mart/uploader.py
+++ innofw/data_mart/uploader.py
@@ -11,6 +11,7 @@ from pydantic import AnyUrl
 from pydantic import validate_arguments
 from pydantic.types import DirectoryPath
 from urlpath import URL
+import hydra
 
 from innofw.constants import S3Credentials
 from innofw.constants import S3FileTags
@@ -24,18 +25,9 @@ from innofw.utils.s3_utils.credentials import get_s3_credentials
 # local modules
 
 
-@validate_arguments
 def upload_dataset(
-    folder_path: DirectoryPath,
-    config_save_path: Path,
-    remote_save_path: AnyUrl,
-    task: Union[str, List[str]],
-    framework: Union[str, List[str]],
-    target: str,
-    name: str,
-    description: str,
-    markup_info: str,
-    date_time: str,
+    dataset_config_path: str,
+    remote_save_path: str,
     access_key: Optional[str] = None,
     secret_key: Optional[str] = None,
     **kwargs,
@@ -43,57 +35,30 @@ def upload_dataset(
     """Function to upload a dataset into s3(remote storage) and generate config file for the dataset
 
     Arguments:
-        folder_path - path to the local folder with `train`, `test` and `infer` inner folders.
-            Can be relative to the project folder path
-        config_save_path - path where to store the config file of the dataset.
-            Can be relative to the project folder path
+        dataset_config_path: path to the dataset config (relatively to config/datasets folder)
         remote_save_path - url to the dataset save location.
             New filename can be specified for the archive file
             Example:
              1. https://api.blackhole.ai.innopolis.university/public-datasets/
              2. https://api.blackhole.ai.innopolis.university/public-datasets/credit_cards.zip
 
-        task - task type which can be solved by this dataset
-        framework - frmaework type used with this datamodule
-        target - target datamodule class
-        name - name of the dataset
-        description - description of the dataset
-        markup_info - info for markup
-        date_time - when data was created
-
         access_key - access key to the remote storage server. If not specified .env will be searched, ~/.aws-credentials
         secret_key - secret key to the remote storage server. If not specified .env will be searched, ~/.aws-credentials
 
     Usage:
 
-    >>> from innofw.zoo import upload_model
+    >>> from innofw.data_mart import upload_dataset
 
-    >>> upload_model(
-    ...     folder_path = "data/industry_data",
-    ...     config_save_path = "config/datasets/industry_data.yaml",
+    >>> upload_dataset(
+    ...     dataset_config_path = "classification/industry_data",
     ...     remote_save_path = "https://api.blackhole.ai.innopolis.university/public-datasets/industry_data.zip",
-    ...     task="text-classification",
-    ...     framework="sklearn",
-    ...     target = "innofw.core.datamodules.lightning_datamodules.detection_coco.CocoLightningDataModule",
-    ...     name = "some name",
-    ...     description = "some description",
-    ...     markup_info =  "something",
-    ...     date_time = "01.01.22",
     ...     access_key = "some key",
     ...     secret_key = "some secret"
     ...     )
 
     in cli:
-        python innofw/data_mart/uploader.py --folder_path data/industry_data\
-                                            --config_save_path config/temp/dataset.yaml\
-                                            --remote_save_path  https://api.blackhole.ai.innopolis.university/public-datasets/test_dataset/\
-                                            --task image-regression\
-                                            --framework torch\
-                                            --target innofw.core.datamodules.lightning_datamodules.detection_coco.CocoLightningDataModule\
-                                            --name "some dataset"\
-                                            --description "something"\
-                                            --markup_info "something"\
-                                            --date_time "something"
+        python innofw/data_mart/uploader.py --dataset_config_path classification/industry_data
+                                            --remote_save_path https://api.blackhole.ai.innopolis.university/public-datasets/test_dataset/\
     """
     if access_key is None or secret_key is None:
         credentials = get_s3_credentials()
@@ -101,16 +66,19 @@ def upload_dataset(
         credentials = S3Credentials(
             ACCESS_KEY=access_key, SECRET_KEY=secret_key
         )
-
+    with hydra.initialize(config_path="../../config", version_base="1.2"):
+        config = hydra.compose(config_name="train.yaml", overrides=[f"datasets={dataset_config_path}"])
+    
     with TemporaryDirectory() as tmpdir:
         tmpdir = Path(tmpdir)
-        remote_paths = dict()
 
         for folder in ["train", "test"]:
             dst_filename = tmpdir / f"{folder}"
             file_path = Path(str(dst_filename) + ".zip")
 
-            shutil.make_archive(str(dst_filename), "zip", folder_path / folder)
+            folder_path = config.datasets.get(folder)["target"]
+
+            shutil.make_archive(str(dst_filename), "zip", Path(folder_path))
 
             # create a tag with hash value
             # compute hash sum and assign to new tag
@@ -126,24 +94,6 @@ def upload_dataset(
             )
             if upload_url is None:
                 raise ValueError("Could not upload the dataset file")
-
-            remote_paths[folder] = {"source": upload_url}
-
-    # config file creation with specified s3 weights path
-    dataset_cfg = DatasetConfig(
-        **kwargs,
-        **remote_paths,
-        _target_=target,
-        task=[task],
-        name=name,
-        description=description,
-        markup_info=markup_info,
-        date_time=date_time,
-        framework=framework,
-    )
-    # save the config file
-    config_save_path = get_abs_path(config_save_path)
-    dataset_cfg.save_as_yaml(config_save_path)
 
     return upload_url
 
