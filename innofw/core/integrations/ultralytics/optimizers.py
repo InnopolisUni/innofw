0069f2e403d19ebdb768e1d8518899efdf13ca7c
MostafaKhaled2017 <m.kira@innopolis.university>
2023-05-12 19:10:06 +0000

    Adapting optimizer for yolov5

--- innofw/core/integrations/ultralytics/optimizers.py
+++ innofw/core/integrations/ultralytics/optimizers.py
@@ -13,11 +13,22 @@ class YOLOV5OptimizerBaseAdapter(BaseAdapter):
 
     def __init__(self):
         self.possible_values = {"optimizer": ["Adam", "AdamW", "SGD"]}
-        self.opt = {"optimizer": "Adam"}
+        self.opt = {"optimizer": "SGD"} # Setting SGD as the default optimizer
         self.hyp = {"momentum": 0.937, "weight_decay": 5e-4}
 
     def adapt(self, optimizer) -> dict:
+        if optimizer is None:
+            return {"opt": self.opt, "hyp": self.hyp}
+        
+        if optimizer._target_.lower().endswith("adam"):
+            self.opt = {"optimizer": "Adam"}
+        elif optimizer._target_.lower().endswith("adamw"):
+            self.opt = {"optimizer": "AdamW"}
+        elif optimizer._target_.lower().endswith("sgd"):
+            self.opt = {"optimizer": "SGD"}
+
         return {"opt": self.opt, "hyp": self.hyp}
+            
 
     def from_cfg(self, cfg):
         return {}, {}
