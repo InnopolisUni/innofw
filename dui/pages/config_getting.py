0d222fea3bb6674be267999e41de499cceafc567
InnopolisUniversity <64022174+InnopolisUni@users.noreply.github.com>
2023-08-25 12:34:32 +0300

    Added dash user interface (#188)
    * Added dash user interface
    
    * Fixed bugs with dui
    
    * Fixed bugs with dui
    
    ---------
    
    Co-authored-by: ibragimmergaliev <i.mergaliev@innopolis.ru>

---
+++ dui/pages/config_getting.py
@@ -0,0 +1,115 @@
+import os
+import dash
+from pathlib import Path
+import dash_bootstrap_components as dbc
+from flask import request, Flask
+app = Flask(__name__)
+import yaml
+import json
+
+configs_path = Path(os.path.join("..", "config"))
+flaskapp = dash.get_app().server
+
+# DO NOT DELETE THIS COMMENTED PIECE OF CODE IT IS NEEDED, BECAUSE WITHOUT IT THIS PAGE IS NOT LOADED ON START
+# AND FLASK WON'T LISTEN TO THE URL
+# dash.register_page(__name__,  path_template="/None")
+
+
+@flaskapp.route("/get_config", methods = ["GET"])
+def get_config():
+    request_body = request.args.get('config_name')
+    request_body += ".yaml" if not request_body.endswith(".yaml") else ""
+    exp_path = configs_path / request_body
+    data = ""
+    if exp_path.exists():
+        with open(exp_path, "r") as yamlfile:
+            try:
+                data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+
+                if data:
+                    data = simplify_dict(data)
+            except yaml.YAMLError as exc:
+                print("ERROR:", exc)
+    return json.dumps({'success': True, 'configuration_parameters': data}), 200, {'ContentType':'application/json'}
+
+
+def parse_config_from_html(config_parameters, recurse=False):
+    config_dict = {}
+    parent = ""
+    for el in config_parameters:
+
+        if el["props"]["className"] == "uniter":
+            # recurse
+            d = parse_config_from_html(el["props"]["children"], recurse=True)
+            config_dict.update(d)
+        elif el["props"]["className"] == "parent":
+            k = el["props"]["children"][1]["props"]["children"]["props"]["value"]
+            v = el["props"]["children"][2]["props"]["children"]["props"]["value"] if len( el["props"]["children"])==4 else None
+            parent = k
+
+            config_dict[k] = [v] if v else []
+        elif el["props"]["className"] == "child":
+            k = el["props"]["children"][1]["props"]["children"]["props"]["value"]
+            v = el["props"]["children"][2]["props"]["children"]["props"]["value"]
+            if not recurse:
+                config_dict[k] = v
+            else:
+                assert parent is not None
+                child = {}
+                child[k] = v
+                config_dict[parent].append(child)
+
+
+    return config_dict
+
+
+def decompose(configurations, recurse=False, level=0):
+
+    configurations_html = []
+    try:
+        for k, v in configurations.items():
+            if type(v) is not dict:
+                configurations_html.append(
+                    dbc.Row([
+
+                            dbc.Col(dbc.Button(className="bi bi-plus rounded-circle", outline=True, color="primary"), width="auto"),
+                        dbc.Col(dbc.Input(className="keyfield", value=k, type="text")),
+                        dbc.Col(dbc.Input(className="valuefield", value=v, type="text")),
+                        dbc.Col(dbc.Button(className="bi bi-pencil", outline=True, color="secondary"), width="auto") if "override /" in k else dbc.Col(width="auto"),
+                        dbc.Col(dbc.Button(className="bi bi-trash", outline=True, color="secondary")),
+                    ], style={"margin-left": 100*level}, className="child")
+                )
+            else:
+
+                children = decompose(v, True, level=level + 1)
+
+                configurations_html.append(
+
+                    dbc.Row([
+                    dbc.Row([
+                        dbc.Col(dbc.Button(className="bi bi-plus rounded-circle", outline=True, color="primary"), width="auto"),
+                        dbc.Col(dbc.Input(className="keyfield", value=k, type="text")),
+                        dbc.Col(dbc.Button(className="bi bi-trash", outline=True, color="secondary")),
+                    ], style={"margin-left": 100*level}, className="parent"),
+                      *children
+                    ], className="uniter"
+                    )
+
+                )
+
+        return configurations_html
+    except Exception as e:
+        print(e, k, v)
+
+def simplify_dict(dict):
+    new_dict = {}
+
+    for k, v in dict.items():
+        if type(v) is list:
+            intermediate_dict = {key: value for element in v for key, value in element.items()}
+            intermediate_dict = simplify_dict(intermediate_dict)
+            new_dict[k] = intermediate_dict
+        else:
+            new_dict[k] = v
+    return new_dict
+
---
+++ dui/pages/config_list.py
@@ -0,0 +1,80 @@
+import os
+import dash
+from pathlib import Path
+import dash_bootstrap_components as dbc
+
+from dash import dash_table, dcc, html
+
+import pandas as pd
+
+dash.register_page(
+    __name__,
+    path='/config_list',
+    title='config_list'
+)
+
+experiment_configs_path = Path(os.path.join("..", "config", "experiments"))
+
+def layout():
+    experiment_configs = []
+    for p in experiment_configs_path.rglob('*'):
+        if str(p).endswith(".yaml"):
+            experiment_configs.append(str(p.relative_to(experiment_configs_path)))
+
+    df = pd.DataFrame().from_dict({"Experiments": experiment_configs})
+
+    df['id'] = list(range(len(df)))
+    df.set_index('id', inplace=True, drop=False)
+
+    for i in range(0, 9999999999):
+        exp_name = f"New Experiment{i}.yaml" if i > 0 else "New Experiment.yaml"
+        p = experiment_configs_path / exp_name
+        if not p.exists():
+            break
+
+
+    config_list = dbc.Container([
+
+        dbc.Row([dbc.Col([html.H4("Experiment Configurator", style={"height": 40})]),
+                 dbc.Col([html.Div(html.Img(src=dash.get_asset_url('_innofw_.svg'), style={"height": 40, "width": 60}),
+                                   className="self-align-right")]),
+                 html.Span(className="border-bottom")],
+
+                style={"margin-top": 10, "margin-bottom": 10}),
+
+
+        dbc.Row([
+        html.Div(id="parent", style={"width": 300}, children=[
+            dcc.Link(dbc.Button('Create new experiment', id='create_exp', style={"width": 300, "margin-bottom": 5}, n_clicks=0),
+                     href=f"/config/{exp_name}", refresh=True),
+
+            dash_table.DataTable(
+                id='table',
+                columns=[
+                    {"name": i, "id": i, "deletable": False, "selectable": False} for i in ["Experiments"]
+                ],
+                data=df.to_dict('records'),
+                editable=False,
+                filter_action="native",
+                filter_options={"placeholder_text": "Search"},
+                sort_action='none',
+                sort_mode="multi",
+                column_selectable="single",
+                row_selectable=False,
+                row_deletable=False,
+                selected_columns=[],
+                selected_rows=[],
+                page_action="native",
+                page_current=0,
+                page_size=10,
+                style_header={'backgroundColor': 'white',
+                              'fontWeight': 'bold',
+                              'textAlign': 'left'
+                              },
+                style_cell={'textAlign': 'left'}
+            ),
+        ]),])
+
+    ])
+
+    return config_list
