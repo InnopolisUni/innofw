2855b5b1dc888ecf8d672e340be53b9501a010b5
Karim Galliamov <45235968+KGallyamov@users.noreply.github.com>
2023-12-06 11:06:52 +0300

    one-shot learning measurement & augmentations bugfix

--- innofw/utils/framework.py
+++ innofw/utils/framework.py
@@ -143,7 +143,7 @@ def get_augmentations(cfg):
         return isinstance(aug, A.Compose) or isinstance(
             aug, A.core.transforms_interface.BasicTransform
         )
-
+    cfg = cfg['augmentations']
     for key in keys:
         try:
             for v in cfg[key].values():
