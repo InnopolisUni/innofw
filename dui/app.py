930ff06f8806904935af3c4dcb8c59f960582de1
InnopolisU <i.mergaliev@innopolis.ru>
2024-05-02 11:00:17 +0300

    Added local js scripts

--- dui/app.py
+++ dui/app.py
@@ -13,9 +13,9 @@ app = dash.Dash(__name__,
                 use_pages=True,
                 external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP, "dui.css"],
                 external_scripts=["dui.js",
-                                    "https://code.jquery.com/jquery-3.3.1.slim.min.js",
-                                    "https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js",
-                                    "https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"])  # initialising dash app
+                                    "jquery-3.3.1.slim.min.js",
+                                    "popper.min.js",
+                                    "bootstrap.min.js"])  # initialising dash app
 dash.register_page(__name__,  path_template="/")
 
 
