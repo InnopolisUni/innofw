8e021ad33cd5d2768bb7295b73c2cd4cea869ced
InnopolisU <i.mergaliev@innopolis.ru>
2024-05-13 21:56:45 +0300

    Fixed error

--- tests/fixtures/config/datasets.py
+++ tests/fixtures/config/datasets.py
@@ -443,3 +443,32 @@ dicomstroke_segmentation_datamodule_cfg_w_target = DictConfig(
     }
 
 )
+
+stroke_segmentation_datamodule_cfg_w_target = DictConfig(
+    {
+        "task": ["image_segmentation"],
+        "name": "stroke_segmentation",
+        "description": "Created by AT&T Laboratories Cambridge",
+        "markup_info": "Информация о разметке",
+        "date_time": "19.07.2022",
+        "_target_": "innofw.core.datamodules.lightning_datamodules.semantic_segmentation.stroke_dm.StrokeSegmentationDatamodule",
+        "train": {
+            "source": str(
+                get_test_folder_path() / "data/images/segmentation/dicom/train"),
+        },
+        "test": {
+            "source": str(
+                get_test_folder_path() / "data/images/segmentation/dicom/test"),
+        },
+        "infer": {
+            "source": str(
+                get_test_folder_path() / "data/images/segmentation/dicom/test"),
+        },
+        "channels_num": 3,
+        "val_size": 0.2,
+        "batch_size": 8,
+        "num_workers": 1,
+        "random_seed": 42,
+    }
+
+)
