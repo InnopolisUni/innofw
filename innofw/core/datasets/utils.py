c588d504dae0755bf382885b136af8e282256697
InnopolisU <i.mergaliev@innopolis.ru>
2024-07-11 11:59:19 +0300

    Added stratified split for mnist classification task

--- innofw/core/datasets/utils.py
+++ innofw/core/datasets/utils.py
@@ -5,6 +5,9 @@ import torch
 
 from innofw.constants import SegDataKeys
 
+import random
+import torch.utils.data
+from collections import defaultdict
 
 def prep_data(image, mask: Optional = None, transform: Optional = None):
     if transform is not None:
@@ -31,3 +34,21 @@ def prep_data(image, mask: Optional = None, transform: Optional = None):
         return {SegDataKeys.image: image, SegDataKeys.label: mask}
 
     return {SegDataKeys.image: image}
+
+
+def stratified_split(dataset : torch.utils.data.Dataset, labels, fraction, random_state=None):
+    if random_state: random.seed(random_state)
+    indices_per_label = defaultdict(list)
+    for index, label in enumerate(labels):
+        indices_per_label[label].append(index)
+    first_set_indices, second_set_indices = list(), list()
+    for label, indices in indices_per_label.items():
+        n_samples_for_label = round(len(indices) * fraction)
+        random_indices_sample = random.sample(indices, n_samples_for_label)
+        first_set_indices.extend(random_indices_sample)
+        second_set_indices.extend(set(indices) - set(random_indices_sample))
+    first_set_inputs = torch.utils.data.Subset(dataset, first_set_indices)
+    first_set_labels = list(map(labels.__getitem__, first_set_indices))
+    second_set_inputs = torch.utils.data.Subset(dataset, second_set_indices)
+    second_set_labels = list(map(labels.__getitem__, second_set_indices))
+    return first_set_inputs, first_set_labels, second_set_inputs, second_set_labels
