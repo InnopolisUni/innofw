89d0f8d8b4c2542dad22199584c67b6d16757663
InnopolisU <i.mergaliev@innopolis.ru>
2024-09-12 23:28:21 +0300

    Removed installation of reqs in runtests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-python -m pip install -U openmim
-python -m pip install -U open3d
-mim install mmengine
-mim install 'mmcv>=2.0.0rc4'
-mim install 'mmdet>=3.0.0'
 export PYTHONPATH=.
 python -m pytest --cov=innofw --cov-report=xml  --junitxml=out_report.xml --cov-fail-under=10 # not percents
 if [ $? -ne 0 ]; then
