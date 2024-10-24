85664303be966781fcf927dbf7a488226e6b93ec
Grigorev Rostislav <grigorjew.rostislaw@gmail.com>
2024-10-24 15:28:48 +0300

    add admet examples

---
+++ examples/admet/toxisity_rabbit_skin_ld50/infer_catboost.sh
@@ -0,0 +1,1 @@
+python infer.py experiments=regression/GR_221024_catboost_toxisity_rabbit_skin_ld50 ckpt_path=https://api.blackhole.ai.innopolis.university/pretrained/catboost_toxisity_rabbit_skin_ld50.pickle
