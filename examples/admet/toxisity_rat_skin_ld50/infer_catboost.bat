9f165d163a256cffddce69f9664a82848a8e27fe
Grigorev Rostislav <grigorjew.rostislaw@gmail.com>
2024-11-05 12:50:41 +0300

    Fix typo in admet experiments

--- examples/admet/toxisity_rat_skin_ld50/infer_catboost.bat
+++ examples/admet/toxisity_rat_skin_ld50/infer_catboost.bat
@@ -1,1 +1,1 @@
-python infer.py experiments=regression/GR_221024_catboost_toxicity_rat_skin_ld50 ckpt_path=https://api.blackhole.ai.innopolis.university/pretrained/catboost_toxisity_rat_skin_ld50.pickle
+python infer.py experiments=regression/GR_221024_catboost_toxisity_rat_skin_ld50 ckpt_path=https://api.blackhole.ai.innopolis.university/pretrained/catboost_toxisity_rat_skin_ld50.pickle
